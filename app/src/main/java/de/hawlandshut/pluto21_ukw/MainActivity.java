a12041834bfc0461293acc52a26d53c51351c0f0
dgreipl <dgreipl@hawlandshutde.onmicrosoft.com>
2020-11-18 12:24:28 +0100

    nach "Delete User" in S4

--- app/src/main/java/de/hawlandshut/pluto21_ukw/MainActivity.java
+++ app/src/main/java/de/hawlandshut/pluto21_ukw/MainActivity.java
@@ -2,7 +2,6 @@ package de.hawlandshut.pluto21_ukw;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -18,6 +17,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import java.util.ArrayList;
 
 import de.hawlandshut.pluto21_ukw.model.Post;
@@ -30,10 +35,10 @@ public class MainActivity extends AppCompatActivity {
     private boolean mIsSignedIn = false;
     private static final String TEST_USERNAME = "Dieter Greipl";
     private static final String TEST_MAIL = "dieter.greipl@gmail.com";
-    private static final String TEST_PASSWORD ="123456";
+    private static final String TEST_PASSWORD = "123456";
 
     // The place to store posts received from server
-    ArrayList<Post>  mPostList = new ArrayList<Post>();
+    ArrayList<Post> mPostList = new ArrayList<Post>();
 
     // Adapter beetween ListView and our list of posts
     ArrayAdapter<Post> mAdapter;
@@ -56,28 +61,28 @@ public class MainActivity extends AppCompatActivity {
                 android.R.layout.simple_list_item_2,
                 android.R.id.text1,
                 mPostList
-        ){
+        ) {
             @NonNull
             @Override
             public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-                Log.d(TAG, "getView called with position " +  position);
-                View view =  super.getView(position, convertView, parent);
+                // Log.d(TAG, "getView called with position " +  position);
+                View view = super.getView(position, convertView, parent);
 
                 TextView text1, text2;
-                text1 = (TextView) view.findViewById( android.R.id.text1);
-                text2 = (TextView) view.findViewById( android.R.id.text2);
+                text1 = (TextView) view.findViewById(android.R.id.text1);
+                text2 = (TextView) view.findViewById(android.R.id.text2);
 
-                Post p = getItem( position );
-                text1.setText( p.title );
-                text2.setText( p.body );
+                Post p = getItem(position);
+                text1.setText(p.title);
+                text2.setText(p.body);
 
                 return view;
             }
         };
 
         // Adapter mit der Listview verbinden
-        mListView =(ListView) findViewById( R.id.listViewMessages );
-        mListView.setAdapter( mAdapter );
+        mListView = (ListView) findViewById(R.id.listViewMessages);
+        mListView.setAdapter(mAdapter);
     }
 
 
@@ -101,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         // TODO: Clean up!
         Intent intent;
-        switch( item.getItemId()) {
+        switch (item.getItemId()) {
             case R.id.mainMenuTestAuthentication:
                 Log.d(TAG, "Test Auth");
                 doTestAuthentication();
@@ -152,22 +157,85 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void doDelete() {
+        // Hole user-Objekt
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            user.delete().addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), "User deleted.", Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "User deletion failed :" + task.getException(), Toast.LENGTH_LONG).show();
+                                Log.d(TAG, "Sign In Fehler " + task.getException());
+                            }
+                        }
+                    });
+        } else {
+            Toast.makeText(getApplicationContext(), "You are not signed in. Pls sign in and delete then.", Toast.LENGTH_LONG).show();
+        }
 
     }
 
     private void doSignOut() {
-
+        // Hole user-Objekt
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
+            Toast.makeText(getApplicationContext(), "No user signed in.",
+                    Toast.LENGTH_LONG).show();
+        } else {
+            FirebaseAuth.getInstance().signOut();
+            Toast.makeText(MainActivity.this, "You are signed out.",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void doSignIn() {
-
+        // Hole user-Objekt
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(TEST_MAIL, TEST_PASSWORD)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), "User signed in.", Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Sign-in Failed :" + task.getException(), Toast.LENGTH_LONG).show();
+                                Log.d(TAG, "Sign In Fehler " + task.getException());
+                            }
+                        }
+                    });
+        } else {
+            Toast.makeText(getApplicationContext(), "You are already signed in. Sign out first.", Toast.LENGTH_LONG).show();
+        }
     }
 
     private void doCreateTestUser() {
-
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(TEST_MAIL, TEST_PASSWORD)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Created User", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "User Creation Failed :" + task.getException(), Toast.LENGTH_LONG).show();
+                            Log.d(TAG, "Create Account Fehler " + task.getException());
+                        }
+                    }
+                });
     }
 
+
     private void doTestAuthentication() {
+        FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (mUser != null) {
+            // User is signed in
+            Toast.makeText(getApplicationContext(), "Signed In:" + mUser.getEmail(), Toast.LENGTH_LONG).show();
+        } else {
+            // No user is signed in
+            Toast.makeText(getApplicationContext(), "Not Signed In", Toast.LENGTH_LONG).show();
+        }
 
     }
 
